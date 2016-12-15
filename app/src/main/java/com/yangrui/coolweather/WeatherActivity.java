3c2971957819784220139fa7d32020b1279b1814
Yang Rui <Yang Rui>
2016-12-15 10:46:03 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/yangrui/coolweather/WeatherActivity.java
+++ app/src/main/java/com/yangrui/coolweather/WeatherActivity.java
@@ -6,11 +6,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -34,7 +39,9 @@ import okhttp3.Response;
  */
 public class WeatherActivity extends AppCompatActivity {
     private ImageView bingPicImg;
-    private SwipeRefreshLayout swipeRefresh;
+    public DrawerLayout drawerLayout;
+    private Button navButton;
+    public SwipeRefreshLayout swipeRefresh;
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -50,6 +57,10 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //取消标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (Build.VERSION.SDK_INT > 21) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
@@ -58,6 +69,8 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weather);
         //初始化控件
         bingPicImg = ((ImageView) findViewById(R.id.bing_pic_img));
+        drawerLayout = ((DrawerLayout) findViewById(R.id.drawer_layout));
+        navButton = ((Button) findViewById(R.id.nav_button));
         swipeRefresh = ((SwipeRefreshLayout) findViewById(R.id.swipe_refresh));
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
         weatherLayout = ((ScrollView) findViewById(R.id.weather_layout));
@@ -97,6 +110,12 @@ public class WeatherActivity extends AppCompatActivity {
                 requestWeather(weatherId);
             }
         });
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     /**
@@ -131,7 +150,7 @@ public class WeatherActivity extends AppCompatActivity {
      * 根据天气id请求城市天气信息
      * @param weatherId
      */
-    private void requestWeather(final String weatherId) {
+    public void requestWeather(final String weatherId) {
         loadBingPic();
         String weatherUrl = "http://guolin.tech/api/weather?cityid="+weatherId+"&key=bc0418b57b2d4918819d3974ac1285d9";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
